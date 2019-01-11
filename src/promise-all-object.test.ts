e2dea08840185a3c591fb4f5debcfc7c03f43195
Inaki Arroyo <inaki.arroyonebreda@gmail.com>
2019-01-11 00:14:09 +0000

    remove quotes

--- src/promise-all-object.test.ts
+++ src/promise-all-object.test.ts
@@ -18,51 +18,51 @@ describe('validate resolved promise', () => {
     const result = await promiseAllObject({
       1: 'resolved',
       '1234-5678': 1234,
-      'test': true,
-      'nullValue': null,
-      'objectValue': {
-        'keyA': 'a'
+      test: true,
+      nullValue: null,
+      objectValue: {
+        keyA: 'a'
       }
     });
 
     expect(result).toEqual({
       1: 'resolved',
       '1234-5678': 1234,
-      'test': true,
-      'nullValue': null,
-      'objectValue': {
-        'keyA': 'a'
+      test: true,
+      nullValue: null,
+      objectValue: {
+        keyA: 'a'
       }
     });
   });
 
   it('should resolve if all values are resolve promises', async () => {
     const result = await promiseAllObject({
-      'promiseA': Promise.resolve('resolved promiseA'),
-      'promiseB': Promise.resolve('resolved promiseB'),
-      'promiseC': Promise.resolve('resolved promiseC'),
-      'promiseD': Promise.resolve('resolved promiseD')
+      promiseA: Promise.resolve('resolved promiseA'),
+      promiseB: Promise.resolve('resolved promiseB'),
+      promiseC: Promise.resolve('resolved promiseC'),
+      promiseD: Promise.resolve('resolved promiseD')
     });
 
     expect(result).toEqual({
-      'promiseA': 'resolved promiseA',
-      'promiseB': 'resolved promiseB',
-      'promiseC': 'resolved promiseC',
-      'promiseD': 'resolved promiseD'
+      promiseA: 'resolved promiseA',
+      promiseB: 'resolved promiseB',
+      promiseC: 'resolved promiseC',
+      promiseD: 'resolved promiseD'
     });
   });
 
   it('should resolve if the values are a mix of promises and non promises', async () => {
     const result = await promiseAllObject({
-      'promiseA': Promise.resolve('resolved promiseA'),
-      'promiseB': Promise.resolve('resolved promiseB'),
+      promiseA: Promise.resolve('resolved promiseA'),
+      promiseB: Promise.resolve('resolved promiseB'),
       1: 'resolved',
-      '1234-5678': 1234,
+      '1234-5678': 1234
     });
 
     expect(result).toEqual({
-      'promiseA': 'resolved promiseA',
-      'promiseB': 'resolved promiseB',
+      promiseA: 'resolved promiseA',
+      promiseB: 'resolved promiseB',
       1: 'resolved',
       '1234-5678': 1234
     });
@@ -73,10 +73,10 @@ describe('validate rejected promise', () => {
   it('should reject if any of the promise rejects', async () => {
     await expect(
       promiseAllObject({
-        'promiseA': Promise.resolve('resolved promiseA'),
-        'promiseB': Promise.resolve('resolved promiseB'),
-        'promiseC': Promise.reject(new Error('reject promiseC')),
-        'promiseD': Promise.resolve('resolved promiseD')
+        promiseA: Promise.resolve('resolved promiseA'),
+        promiseB: Promise.resolve('resolved promiseB'),
+        promiseC: Promise.reject(new Error('reject promiseC')),
+        promiseD: Promise.resolve('resolved promiseD')
       })
     ).rejects.toThrowError();
   });
@@ -84,7 +84,7 @@ describe('validate rejected promise', () => {
   it('should reject if any of the promise rejects with mix values types', async () => {
     await expect(
       promiseAllObject({
-        'promiseA': Promise.resolve('resolved promiseA'),
+        promiseA: Promise.resolve('resolved promiseA'),
         1: Promise.reject(new Error('reject promise with number as key'))
       })
     ).rejects.toThrowError();
@@ -93,10 +93,10 @@ describe('validate rejected promise', () => {
   it('should reject if all the promise rejects', async () => {
     await expect(
       promiseAllObject({
-        'promiseA': Promise.reject(new Error('resolved promiseA')),
-        'promiseB': Promise.reject(new Error('resolved promiseB')),
-        'promiseC': Promise.reject(new Error('reject promiseC')),
-        'promiseD': Promise.reject(new Error('resolved promiseD'))
+        promiseA: Promise.reject(new Error('resolved promiseA')),
+        promiseB: Promise.reject(new Error('resolved promiseB')),
+        promiseC: Promise.reject(new Error('reject promiseC')),
+        promiseD: Promise.reject(new Error('resolved promiseD'))
       })
     ).rejects.toThrowError();
   });
