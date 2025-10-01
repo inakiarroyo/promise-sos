978eed9df480266f2b766552c9642c3ac0890563
Iñaki Arroyo <dev@inakiarroyo.com>
2025-10-01 19:06:46 +0200

    fix: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+/** @type {import('jest').Config} */
 module.exports = {
   verbose: false,
   automock: false,
@@ -6,24 +7,24 @@ module.exports = {
     '!<rootDir>/node_modules/',
     '!<rootDir>/src/**/index.ts',
     '!<rootDir>/**/*.d.ts',
-    '<rootDir>/src/**/*.{ts,tsx,js,jsx}'
+    '<rootDir>/src/**/*.{ts,tsx,js,jsx}',
   ],
   roots: ['<rootDir>/src'],
   modulePaths: ['<rootDir>/src/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   testPathIgnorePatterns: ['/node_modules/', '<rootDir>/build'],
   coverageThreshold: {
     global: {
       branches: 100,
       functions: 100,
       lines: 100,
-      statements: 100
-    }
+      statements: 100,
+    },
   },
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.(t|j)sx?$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   testURL: 'http://localhost/',
-  testEnvironment: 'jsdom'
-}
+  testEnvironment: 'jsdom',
+};
